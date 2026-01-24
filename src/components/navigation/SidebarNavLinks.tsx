f7c1a156bf57de4ad2333cf5befefe270c164390
Mohsen Parsa <manai.raghavenderr@gmail.com>
2026-01-24 03:04:06 -0900

    feat: init left sidebar

---
+++ src/components/navigation/SidebarNavLinks.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import { SheetClose } from "../ui/sheet";
+import { cn } from "@/lib/utils";
+import { NavLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+
+export default function SidebarNavLinks() {
+  const pathName = usePathname();
+
+  return (
+    <ul className="px-4 flex flex-col space-y-4">
+      {NavLinks.map((nav) => {
+        const isActive = nav.href == pathName;
+
+        return (
+          <SheetClose asChild key={nav.id}>
+            <Link href={nav.href}>
+              <li
+                className={cn(
+                  "p-3 rounded-lg flex gap-4 text-white items-center",
+                  isActive && "primary_gradient",
+                )}
+              >
+                <nav.icon className="size-7" />
+                <span>{nav.label}</span>
+              </li>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </ul>
+  );
+}
