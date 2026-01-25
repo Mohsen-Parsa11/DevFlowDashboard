611f96deee1765b561680e84f595cc0bcbf03d43
Mohsen Parsa <manai.raghavenderr@gmail.com>
2026-01-25 04:31:24 -0900

    feat: add popular tag

--- src/components/navigation/LeftSidebar.tsx
+++ src/components/navigation/LeftSidebar.tsx
@@ -3,29 +3,48 @@ import { NavLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Sheet, SheetClose, SheetFooter } from "../ui/sheet";
+import { Button } from "../ui/button";
+import ROUTES from "@/constants/route";
 
 export default function LeftSidebar() {
   const pathName = usePathname();
 
   return (
-    <ul className="px-4 flex-col space-y-4 bg-slate-900 min-h-screen hidden md:flex">
-      {NavLinks.map((nav) => {
-        const isActive = nav.href == pathName;
+    <Sheet>
+      <ul className="px-4 flex-col space-y-4 bg-slate-900 min-h-screen hidden md:flex">
+        {NavLinks.map((nav) => {
+          const isActive = nav.href == pathName;
 
-        return (
-          <Link href={nav.href} key={nav.id}>
-            <li
-              className={cn(
-                "p-3 rounded-lg flex gap-4 text-white items-center",
-                isActive && "primary_gradient",
-              )}
-            >
-              <nav.icon className="size-5" />
-              <span className="hidden lg:block">{nav.label}</span>
-            </li>
-          </Link>
-        );
-      })}
-    </ul>
+          return (
+            <Link href={nav.href} key={nav.id}>
+              <li
+                className={cn(
+                  "p-3 rounded-lg flex gap-4 text-white items-center",
+                  isActive && "primary_gradient",
+                )}
+              >
+                <nav.icon className="size-5" />
+                <span className="hidden lg:block">{nav.label}</span>
+              </li>
+            </Link>
+          );
+        })}
+        <div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button className="w-full bg-slate-800 hover:bg-slate-800/80 cursor-pointer border border-gray-700 text-primary-400 ">
+                <Link href={ROUTES.SIGN_IN}>log in</Link>
+              </Button>
+            </SheetClose>
+            <SheetClose asChild>
+              <Button variant="outline" className="cursor-pointer w-full">
+                <Link href={ROUTES.SIGN_UP}>sign up</Link>
+              </Button>
+            </SheetClose>
+          </SheetFooter>
+        </div>
+      </ul>
+    </Sheet>
   );
 }
--- src/components/navigation/RightSidebar.tsx
+++ src/components/navigation/RightSidebar.tsx
@@ -1,4 +1,5 @@
 import { ChevronRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const hotQuestions = [
@@ -10,19 +11,71 @@ const hotQuestions = [
   { id: 6, title: "How to use useContext?" },
 ];
 
+const popularTags = [
+  {
+    id: 1,
+    tagName: "REACT",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    showCount: 100,
+  },
+  {
+    id: 2,
+    tagName: "JAVASCRIPT",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    showCount: 200,
+  },
+  {
+    id: 3,
+    tagName: "NEXT",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    showCount: 300,
+  },
+  {
+    id: 4,
+    tagName: "TYPESCRIPT",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    showCount: 400,
+  },
+  {
+    id: 5,
+    tagName: "REACT-QUERY",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg",
+    showCount: 500,
+  },
+];
 export default function RightSidebar() {
   return (
     <div className="py-10  px-6 bg-slate-900 min-h-screen">
-      <h2 className="font-medium text-xl">Top Questions</h2>
+      <div>
+        <h2 className="font-medium text-xl">Top Questions</h2>
+        <Link href="#" className="mt-6 flex flex-col gap-4">
+          {hotQuestions.map((q) => (
+            <div className="flex items-center justify-between" key={q.id}>
+              <p className="text-sm">{q.title}</p>
+              <ChevronRight />
+            </div>
+          ))}
+        </Link>
+      </div>
 
-      <Link href="#" className="mt-6 flex flex-col gap-4">
-        {hotQuestions.map((q) => (
-          <div className="flex items-center justify-between" key={q.id}>
-            <p className="text-sm">{q.title}</p>
-            <ChevronRight />
+      <div className=" mt-14">
+        <h2 className="font-medium text-xl">Popular Tags</h2>
+        {popularTags.map(({ id, icon, tagName, showCount }) => (
+          <div key={id} className="flex items-center justify-between mt-6">
+            <div className="px-1.5 py-1 flex items-center gap-1 text-sm bg-slate-800 rounded-md">
+              <Image
+                src={icon}
+                alt="icon"
+                width={24}
+                height={24}
+                className=""
+              />
+              <span className="">{tagName}</span>
+            </div>
+            <p className="font-medium">{showCount}</p>
           </div>
         ))}
-      </Link>
+      </div>
     </div>
   );
 }
